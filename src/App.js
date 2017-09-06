38a57d8accf2112a3d5cfdf12e73ae86fbcbeb65
Tyler Garlick <tjgarlick@gmail.com>
2017-09-06 09:59:07 -0600

    Sample localStorage
    - Added configuration
      - Added Localstorage configuration
    - Added yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Routing from './Routing';
+import { storage } from './configuration'
 
 
 class App extends Component {
   render() {
+
+    // storage.save('blah', { message: 'hello'})
+    // const value = storage.get('blah')
+    // console.log(value)
+
     return (
       <main>
         <Routing />
